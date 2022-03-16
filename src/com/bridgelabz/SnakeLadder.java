5b98017a7924c520a216098a530c22e160eab8f3
AdeshMask <adeshmaske255@gmail.com>
2022-03-16 13:25:50 +0530

    Added:Check for options

--- src/com/bridgelabz/SnakeLadder.java
+++ src/com/bridgelabz/SnakeLadder.java
@@ -10,9 +10,26 @@ public class SnakeLadder {
         int min = 1;
         int max = 6;
         System.out.println(+playerPosition+" " + "This is the players starting Position");
-        System.out.println("Roll the dice to get number ");
+        System.out.println("Rolls the dice to get number ");
         int number = (int) Math.floor(Math.random()*(max-min+1)+min);
         System.out.println(number);
+
+        int option = (int) Math.floor(Math.random()*3);
+        if (option == 0) {
+            System.out.println("No Play");
+            playerPosition = playerPosition;
+            System.out.println(playerPosition);
+        }
+        else if (option == 1) {
+            System.out.println("Ladder");
+            playerPosition = playerPosition + number;
+            System.out.println(playerPosition);
+        }
+        else {
+            System.out.println("Snake");
+            playerPosition = playerPosition - number;
+            System.out.println(playerPosition);
+        }
 
     }
 }
