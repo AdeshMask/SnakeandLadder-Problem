da662b257e9a08edcb2ca13d6104bc653d641ccc
AdeshMask <adeshmaske255@gmail.com>
2022-03-16 12:38:24 +0530

    Welcome to Snake and Ladder

---
+++ src/com/bridgelabz/SnakeLadder.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+import java.util.Random;
+
+public class SnakeLadder {
+
+    public static void main(String[] args) {
+	// write your code here
+        int playerPosition = 0;
+        System.out.println(+playerPosition+"This is the players starting Position");
+        int number = (int) Math.floor(Math.random()*7);
+        System.out.println(number);
+    }
+}
